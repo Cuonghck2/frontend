a346c692f2cf0f67bb052bb74bd406369d264b19
Duc Cuong <cuonghc2k2abc@gmail.com>
2023-12-23 21:44:35 +0700

    build Ui admin

--- src/page/Admin/updateTopic/UpdateTopic.jsx
+++ src/page/Admin/updateTopic/UpdateTopic.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useMemo } from 'react'
 import { Add, Delete, Edit, Search } from '@mui/icons-material'
 import {
-    Button, Divider, FormControl, IconButton, InputBase, InputLabel, MenuItem, Paper,
+    Box,
+    Button, Divider, FormControl, IconButton, InputBase, InputLabel, MenuItem, Modal, Paper,
     Select, Table, TableBody, TableContainer,
-    TableHead, TablePagination, TableRow
+    TableHead, TablePagination, TableRow, Typography
 } from '@mui/material'
 import { StyleTableCell, StyledTableRow } from '../../../Layouts/component/customMUI/customMUI';
 import { useState } from 'react';
@@ -23,6 +24,7 @@ const UpdateTopic = () => {
     const [searchResult, setSearchResult] = useState([])
     const [searchValue, setSearchValue] = useState('')
     const [openModal, setOpenModal] = useState(false);
+    const [confirmDel, setConfirmDel] = useState(false)
     const dispatch = useDispatch()
     const { deleTopic } = useTopic()
     useEffect(() => {
@@ -47,6 +49,7 @@ const UpdateTopic = () => {
     const handleDeleteTopic = (id) => {
         dispatch(deleteTopic(id))
         deleTopic(id)
+        setConfirmDel(false)
     }
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -71,10 +74,15 @@ const UpdateTopic = () => {
     };
     const handleOpenModal = () => {
         setOpenModal(true);
-
     }
     const handleCloseModal = () => {
         setOpenModal(false);
+    }
+    const handleConfirmDel = () => {
+        setConfirmDel(true)
+    }
+    const handleCancelDel = () => {
+        setConfirmDel(false)
     }
     return (
         <>
@@ -127,7 +135,6 @@ const UpdateTopic = () => {
                                 ? searchResult.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                                 : topics.topics
                             ).map((topic, index) => {
-                                console.log(topic)
                                 return (
                                     <StyledTableRow key={index}>
                                         <StyleTableCell align="center">{topic.data.data?.idTopic}</StyleTableCell>
@@ -145,9 +152,33 @@ const UpdateTopic = () => {
                                                 setModalData(topic)
                                                 handleOpenModal()
                                             }}><Edit /></IconButton>
-                                            <IconButton color='primary' size='medium' variant="text" onClick={() => { handleDeleteTopic(topic?.id) }}><Delete /></IconButton>
+                                            <IconButton color='primary' size='medium' variant="text" onClick={handleConfirmDel}><Delete /></IconButton>
                                         </StyleTableCell>
-
+                                        <Modal
+                                            onClose={handleCancelDel}
+                                            keepMounted
+                                            open={confirmDel}
+                                        >
+                                            <Box
+                                                sx={{
+                                                    position: 'absolute',
+                                                    top: '50%',
+                                                    left: '50%',
+                                                    transform: 'translate(-50%, -50%)',
+                                                    width: 600,
+                                                    bgcolor: '#fff',
+                                                    boxShadow: 24,
+                                                    borderRadius: "12px",
+                                                    padding: 4,
+                                                }}
+                                            >
+                                                <Typography variant="h5" sx={{ marginBottom: "15px" }} >
+                                                    Bạn có chắc chắn muốn xóa hay không ?
+                                                </Typography>
+                                                <Button variant="text" sx={{ margin: "24px 0", float: "right" }} onClick={handleCancelDel}>Hủy Bỏ</Button>
+                                                <Button onClick={() => { handleDeleteTopic(topic?.id) }} variant="contained" sx={{ margin: "24px 12px", float: "right" }}>Xóa</Button>
+                                            </Box>
+                                        </Modal>
                                     </StyledTableRow>
                                 )
                             }) : <></>}
